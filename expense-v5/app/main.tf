2220c3918515479ec8c5d331b5ee4d6b055ea2e1
sai <kuppalasaivikram@gmail.com>
2024-06-09 16:00:37 +0530

    adding provisioner for localexec

--- expense-v5/app/main.tf
+++ expense-v5/app/main.tf
@@ -6,6 +6,9 @@ resource "aws_instance" "node" {
   tags = {
     name = var.name
   }
+  provisioner "local-exec" {
+    command = "sleep 120; cd /home/ec2-user/expense-ansible ; ansible-playbook -i ${self.private_ip}, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${var.name} -e env=dev expense.yml"
+  }
 }
 resource "aws_route53_record" "record" {
   zone_id = var.zone_id
