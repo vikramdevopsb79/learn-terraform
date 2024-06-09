21edb12e64acc50e29afd0b6d7155a312997f5b4
sai <kuppalasaivikram@gmail.com>
2024-06-09 15:17:17 +0530

    security sending as list instead of string

--- expense-v5/main.tf
+++ expense-v5/main.tf
@@ -4,6 +4,6 @@ module "source" {
   ami           = data.aws_ami.ami.image_id
   instance_type =  each.value["instance_type"]
   name = each.key
-  vpc_security_group_ids = data.aws_security_group.allow-all.id
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
   zone_id = data.aws_route53_zone.main.zone_id
 }
