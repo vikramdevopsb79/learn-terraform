a728a5ebbe5abd8988c848b701d13763d823c362
sai <kuppalasaivikram@gmail.com>
2024-07-01 15:03:23 +0530

    terraform variables learn

---
+++ 02-variables/main.tf
@@ -0,0 +1,40 @@
+variable "y" {}
+output "y" {
+  value = var.y
+}
+
+variable "x" {
+  default = 10
+}
+output "x" {
+  value = var.x
+}
+variable "url" {}
+output "url" {
+  value = var.url
+}
+variable "l" {
+  default = [
+    10,
+    20,
+    false,
+    "vikram"
+  ]
+}
+output "l" {
+  value = var.l[2]
+}
+output "concatenated_values" {
+  value = "${tostring(var.l[0])}, ${tostring(var.l[1])}, ${tostring(var.l[2])}, ${var.l[3]}"
+  description = "Concatenated values from the list variable l"
+}
+variable "m" {
+  default = {
+    course = "Devops"
+    trainer = "sai"
+  }
+}
+output "m" {
+  value = var.m["course "]
+  description = "course details are getting properly"
+}
