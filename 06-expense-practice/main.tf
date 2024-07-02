b3d5b6392c66a7f4003e943b4773872295b1be47
sai <kuppalasaivikram@gmail.com>
2024-07-02 09:30:38 +0530

    set of string required for security groups

--- 06-expense-practice/main.tf
+++ 06-expense-practice/main.tf
@@ -3,7 +3,7 @@ module "source" {
   source = "./sample"
   ami           = data.aws_ami.ami.id
   instance_type = each.value["instance_type"]
-  vpc_security_group_ids = data.aws_security_group.selected.id
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   name = each.key
   zone_id = data.aws_route53_zone.main.zone_id
 }
